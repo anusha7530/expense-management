c0c99cf97d7ca54d35e737ed7b013eb465fe01b6
anusha7530 <agarwalanusha13@gmail.com>
2024-09-08 23:39:27 +0530

    Added login and register page

--- server.js
+++ server.js
@@ -13,12 +13,9 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
+app.use('/api/v1/users',require('./routes/userRoute'));
 
 const PORT = 8080 || process.env.PORT;
-
-app.get("/",(req,res) => {
-    res.send("Api is Working");
-})
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
