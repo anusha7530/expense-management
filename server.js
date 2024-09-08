fcfdd12599363adb394681399cd793fa6847dfac
anusha7530 <agarwalanusha13@gmail.com>
2024-09-08 17:20:44 +0530

    Initialised backend

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const colors = require("colors");
+
+dotenv.config();
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors());
+
+const PORT = 8080 || process.env.PORT;
+
+app.get("/",(req,res) => {
+    res.send("Api is Working");
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
